13df5f1e68182840579495956f77c60c22ac437c
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-09-24 00:02:34 +0100

    Add barrels pattern

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as FormWizard } from './FormWizard.vue';
+export { default as FormUserDetails } from './FormUserDetails.vue';
+export { default as FormReviewOrder } from './FormReviewOrder.vue';
+export { default as FormPlanPicker } from './FormPlanPicker.vue';
+export { default as FormAddress }from './FormAddress.vue';
