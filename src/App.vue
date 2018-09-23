86f8f9cd6877ce93dee712806eb3c1e364fc9547
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-09-23 23:29:29 +0100

    Update App.vue with FormWizard

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app" class="container mx-auto py-6">
+    <header>
+      <div>
+        <span id="logo">Liquid Gold Box</span>
+      </div>
+    </header>
+
+    <div class="content">
+      <form-wizard/>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import FormWizard from './components/FormWizard.vue'
 
-export default Vue.extend({
+export default {
   name: 'app',
   components: {
-    HelloWorld
+    FormWizard
   }
-});
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
