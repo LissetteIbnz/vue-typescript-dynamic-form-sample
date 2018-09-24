0c62cd6e30803988a68c94f5fe8727e1149c5b29
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-09-24 22:50:27 +0100

    Refactor code

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,39 +3,44 @@ import { UserEntity } from '@/models';
 const user: UserEntity = {
   email: 'john@travolta.io',
   password: '123456',
-  name: 'John'
-}
+  name: 'John',
+};
 
-export const checkIfUserExistsInDB = (email: string): Promise<boolean | string> => {
+export const checkIfUserExistsInDB = (
+  email: string,
+): Promise<boolean | string> => {
   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (email === user.email) {
-          resolve(true)
-        } else {
-          reject('user not found')
-        }
-      }, 1000)
-  })
-}
+    setTimeout(() => {
+      if (email === user.email) {
+        resolve(true);
+      } else {
+        reject('user not found');
+      }
+    }, 1000);
+  });
+};
 
-export const authenticateUser = (email: string, password: string): Promise<Object | string> => {
+export const authenticateUser = (
+  email: string,
+  password: string,
+): Promise<Object | string> => {
   return new Promise((resolve, reject) => {
-    console.log(email === user.email && password === user.password)
+    console.log(email === user.email && password === user.password);
     setTimeout(() => {
       if (email === user.email && password === user.password) {
         resolve({
           email: user.email,
-          name: user.name
-        })
+          name: user.name,
+        });
       } else {
-        reject('supplied credentials do not match any user')
+        reject('supplied credentials do not match any user');
       }
-    }, 1000)
-  })
-}
+    }, 1000);
+  });
+};
 
 export const postFormToDB = (form: Object): Promise<Object> => {
-  return new Promise((resolve,) => {
-    setTimeout(() => resolve(form), 1000)
-  })
-}
+  return new Promise(resolve => {
+    setTimeout(() => resolve(form), 1000);
+  });
+};
